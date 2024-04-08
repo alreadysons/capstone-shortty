04d4b0e3e050f19acc09e205e8085bbe730df007
alreadysons <alreadysons@gmail.com>
2024-04-08 23:19:07 +0900

    보안문제로 인한 gitignore 세팅

--- shortty/shortty/settings.py
+++ shortty/shortty/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t-1&q&oqer9v5my1_253su#s%2z2p5dwvd(d^mkywxr5syfsqp"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,8 +78,16 @@ WSGI_APPLICATION = "shortty.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": config("DATABASE_NAME"),  # Here
+        "USER": config("DATABASE_USERNAME"),  # Here
+        "PASSWORD": config("DATABASE_PASSWORD"),  # Here
+        "HOST": config("DATABASE_HOST"),  # Here
+        "PORT": config("DATABASE_PORT", cast=int),  # Here
+        "OPTIONS" : {
+            'init_command' : 'SET sql_mod="STRICT_TRANS_TABELS"'
+        }
+        
     }
 }
 
