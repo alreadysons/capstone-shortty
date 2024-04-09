8c79e54f16a7ad9a951738c1f25941595ba4ebc5
alreadysons <alreadysons@gmail.com>
2024-04-09 21:07:23 +0900

    setting 3차변경

--- shortty/shortty/settings.py
+++ shortty/shortty/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-t-1&q&oqer9v5my1_253su#s%2z2p5dwvd(d^mkywxr5syfsqp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
